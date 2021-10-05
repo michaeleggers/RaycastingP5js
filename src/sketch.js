089cdfad58801252fd90ef8158714e591203e06a
Michael Eggers <mseggers@protonmail.ch>
2021-10-05 22:09:09 +0200

    add p5js library and init source

---
+++ src/sketch.js
@@ -0,0 +1,99 @@
+const map = [
+  'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w',
+  'w', '_', '_', '_', '_', 'w', '_', '_', '_', 'w',
+  'w', '_', '_', '_', '_', 'w', '_', '_', '_', 'w',
+  'w', 'w', 'w', '_', '_', 'w', '_', '_', '_', 'w',
+  'w', '_', '_', '_', '_', '_', '_', '_', '_', 'w',
+  'w', '_', '_', '_', '_', 'w', '_', '_', '_', 'w',
+  'w', '_', '_', '_', '_', '_', '_', '_', '_', 'w',
+  'w', '_', '_', '_', '_', '_', '_', '_', '_', 'w',
+  'w', '_', '_', '_', '_', '_', '_', '_', '_', 'w',
+  'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', '_', 'w'
+]
+
+class Player {
+  constructor(pos, dir) {
+    this.pos = pos
+    this.dir = dir
+  }
+
+  draw() {
+    fill('red')
+    circle(this.pos.x, this.pos.y, 10)
+    stroke('red')
+    strokeWeight(3)
+    line(this.pos.x, this.pos.y, this.pos.x + 100*this.dir.x, this.pos.y + 100*this.dir.y)
+  }
+}
+
+let player
+
+function index(x, y) {
+  return y * 10 + x
+}
+
+function updatePlayer() {
+  if (keyIsDown(LEFT_ARROW)) {
+    player.dir.rotate(-0.1)
+  }
+  if (keyIsDown(RIGHT_ARROW)) {
+    player.dir.rotate(0.1)
+  }
+  if (keyIsDown(UP_ARROW)) {
+    player.pos.add(p5.Vector.mult(player.dir, 2.0))
+  }
+  if (keyIsDown(DOWN_ARROW)) {
+    player.pos.sub(p5.Vector.mult(player.dir, 2.0))
+  }
+}
+
+function drawMap2D() {
+  for (let y=0; y<10; y++) {
+    for (let x=0; x<10; x++) {
+      let tile = map[index(x, y)]
+      if (tile === 'w') {
+        fill(255)
+      }
+      else {
+        fill(80)
+      }
+      stroke('black')
+      strokeWeight(1)
+      rect(32*x % 320, 32*y, 32, 32)
+    }
+  }
+}
+
+function raycast(pos, dir) {
+  // current pos within tile
+  let tilePos = createVector(pos.x % 32, pos.y % 32)
+  
+  // first hitpoint
+  let dx = 32.0 - tilePos.x
+  let dy = 32.0 - tilePos.y
+  let firstVerticalX = pos.x + dx
+  let m = dir.y / dir.x
+  let firstVerticalY = pos.y + m*dx
+  fill('orange')
+  strokeWeight(0)
+  circle(firstVerticalX, firstVerticalY, 10)
+  circle(firstVerticalX + 32.0, firstVerticalY + m*32.0, 10)
+}
+
+function setup() {
+  // put setup code here
+  frameRate(30)
+
+  createCanvas(640, 600)
+
+  player = new Player(createVector(110.0, 100.0), createVector(1.0, 0.0))
+}
+
+function draw() {
+  background(0)
+
+  updatePlayer()
+  drawMap2D()
+  player.draw()
+  raycast(player.pos, player.dir)
+}
